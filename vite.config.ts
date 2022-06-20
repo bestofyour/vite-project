803026830612998cf3a9b455a19121c21ec35731
bestofyour <964762268@qq.com>
2022-06-20 14:38:58 +0800

    feat: stylelint

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
-import {resolve} from 'path'
+import { resolve } from 'path'
 function pathResolve(dir) {
-  return resolve(__dirname, ".", dir);
+    return resolve(__dirname, '.', dir)
 }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(), 
-    eslintPlugin({
-      include: ["src/**/*.vue", "src/**/*.ts", "src/**/*.tsx"]
-    })
-  ],
-  resolve: {
-    alias: {
-      "@": pathResolve("src"),
-    },
+    plugins: [
+        vue(),
+        eslintPlugin({
+            include: ['src/**/*.vue', 'src/**/*.ts', 'src/**/*.tsx'],
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': pathResolve('src'),
+        },
 
-    extensions: ['.vue', '.ts', '.tsx']
-  },
+        extensions: ['.vue', '.ts', '.tsx'],
+    },
 })
