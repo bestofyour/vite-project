cbec8121406a2e09e0fc7e428179f6ab80481904
bestofyour <964762268@qq.com>
2022-06-20 14:31:40 +0800

    feat: stylelint

--- types/global.d.ts
+++ types/global.d.ts
@@ -5,39 +5,37 @@ import type {
     ComponentPublicInstance,
     FunctionalComponent,
     PropType as VuePropType,
-  } from 'vue';
+} from 'vue'
 
 declare global {
-    declare type Recordable<T = any> = Record<string, T>;
+    declare type Recordable<T = any> = Record<string, T>
 
-    declare type PropType<T> = VuePropType<T>;
+    declare type PropType<T> = VuePropType<T>
 
     interface ImportMetaEnv extends ViteEnv {
-        __: unknown;
-      }
-    
+        __: unknown
+    }
+
     declare interface ViteEnv {
-        VITE_PORT: number;
-        VITE_USE_MOCK: boolean;
-        VITE_USE_PWA: boolean;
-        VITE_PUBLIC_PATH: string;
-        VITE_PROXY: [string, string][];
-        VITE_GLOB_APP_TITLE: string;
-        VITE_GLOB_APP_SHORT_NAME: string;
-        VITE_USE_CDN: boolean;
-        VITE_DROP_CONSOLE: boolean;
-        VITE_BUILD_COMPRESS: 'gzip' | 'brotli' | 'none';
-        VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE: boolean;
-        VITE_LEGACY: boolean;
-        VITE_USE_IMAGEMIN: boolean;
-        VITE_GENERATE_UI: string;
+        VITE_PORT: number
+        VITE_USE_MOCK: boolean
+        VITE_USE_PWA: boolean
+        VITE_PUBLIC_PATH: string
+        VITE_PROXY: [string, string][]
+        VITE_GLOB_APP_TITLE: string
+        VITE_GLOB_APP_SHORT_NAME: string
+        VITE_USE_CDN: boolean
+        VITE_DROP_CONSOLE: boolean
+        VITE_BUILD_COMPRESS: 'gzip' | 'brotli' | 'none'
+        VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE: boolean
+        VITE_LEGACY: boolean
+        VITE_USE_IMAGEMIN: boolean
+        VITE_GENERATE_UI: string
     }
 }
 
-
-
 declare module 'vue' {
     export type JSXComponent<Props = any> =
-      | { new (): ComponentPublicInstance<Props> }
-      | FunctionalComponent<Props>;
+        | { new (): ComponentPublicInstance<Props> }
+        | FunctionalComponent<Props>
 }
