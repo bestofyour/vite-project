3dc866aa6f95945dde3397625b8cbea0259a486a
bestofyour <964762268@qq.com>
2022-07-01 17:55:34 +0800

    fix: 666

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,4 +1,5 @@
 <template>
+  <div class="text-red-300 div bg-red-900 hover:bg-blue-900">111111</div>
   <Button type="dashed" class="text-pink-50 hover:text-red-900">11</Button>
 </template>
 
@@ -9,3 +10,11 @@
   const { title } = useScroll();
   console.log('title', title);
 </script>
+
+<style lang="less" scoped>
+  .div {
+    color-scheme: dark;
+    font-display: auto;
+    font-weight: bold;
+  }
+</style>
